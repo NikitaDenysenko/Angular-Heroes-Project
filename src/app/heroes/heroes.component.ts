a7d05f9937818e7a98122b22bbf9eeb45f2fe3ed
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-03-15 22:01:48 +0200

    added listing of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-import {Hero} from '../hero'
+import { Hero } from '../hero'
 
+import { HEROES } from '../mock-heroes'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,6 +12,9 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   }
+
+  heroes: Hero[] = [...HEROES]
+
   constructor () {}
 
   ngOnInit (): void {}
